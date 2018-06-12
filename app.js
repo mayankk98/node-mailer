50c78b85be8499a312a39a92b91032cbcaabf897
Mayank Khandelwal <mayankkhandelwal92@gmail.com>
2018-06-12 23:30:10 +0530

    Added routes and nodemailer

--- app.js
+++ app.js
@@ -17,7 +17,57 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+	res.render('contact');
+});
 
+app.post('/send', (req, res) => {
+	const output = `
+		<p>You have a new contact request</p>
+		<h3>Contact Details</h3>
+		<ul>
+			<li>Name: ${req.body.name} </li>
+			<li>Company: ${req.body.company} </li>
+			<li>Email: ${req.body.email} </li>
+			<li>Phone: ${req.body.phone} </li>
+		</ul>
+		<h3>Message</h3>
+		<p>${req.body.message}</p>`;
+
+	// create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: //'mail.google.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: //test@mayankkhandelwal.com 
+            pass: //whatever you set for your account 
+        },
+        tls:{
+        	rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <test@mayankkhandelwal.com>', // sender address
+        to: 'whoever@example.com', // list of receivers 
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', {msg:'Email has been sent'});
+    });
+});
 
 
 app.listen(3000, () => console.log('Server started..'));
